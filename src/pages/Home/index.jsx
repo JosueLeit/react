9b0b0ce5b8d981322199203286408df4c1d41247
JosueLeit <josueleite@outlook.com>
2022-06-03 20:39:37 -0300

    creating useState

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,26 @@
 import './styles.css';
+import logo_horizontal from"../../images/logo_horizontal.png";
 import { Card } from '../../components/Card';
+import React, { useState } from 'react';
 
 export function Home() {
+
+  const [clientName, setClientName] = useState();
+
+  
+
   return (
   <div className="container">
-  <h1>Lista de Presença</h1>
-  <input type="text" placeholder="Digite o nome"/>
+  <img className="logo" src={logo_horizontal} alt="" />
+  <input 
+  type="text" 
+  placeholder="Digite o seu nome para reservar"
+  onChange={ e => setClientName(e.target.value)}
+  />
   <button type="button">Adicionar</button>
   
-  <Card />
-  <Card />
-  <Card />
+  <Card name={clientName} time="19:29:25"/>
+  <Card name="Kradepeido" time="10:19:35"/>
   </div>
 )
 }
